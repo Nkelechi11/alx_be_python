bf1c01d25bf3c3290fb17881a440a9251ecc2160
Clement Adjavon <118020602+Nkelechi11@users.noreply.github.com>
2025-05-28 08:36:47 +0000

    Update multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -6,10 +6,19 @@
 #     # print(number, '*', input, "=", number*input)
 
 
-# Ask the user to enter a number
-number = int(input("Enter a number to see its multiplication table: "))
+# # Ask the user to enter a number
+# number = int(input("Enter a number to see its multiplication table: "))
 
-# Use a for loop to print the multiplication table from 1 to 10
-for i in range(1, 11):
-    product = number * i
-    print(f"{number} * {i} = {product}")
+# # Use a for loop to print the multiplication table from 1 to 10
+# for i in range(1, 11):
+#     product = number * i
+#     print(f"{number} * {i} = {product}")
+
+number = int(input(" Enter a number to see its multiplication table: "))
+
+for input in range(0,11):
+    result = number * input
+    print(f"{number} * {input} = {result}")
+    # print(number, '*', input, "=", number*input)
+
+
