d8b8716e2efc148f1840ad6a8a3816a2949eaed0
Adjavon Clement <adjavonkwabena20@gmail.com>
2025-06-14 15:58:08 +0000

    bank_account.py udate

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -12,7 +12,9 @@ class BankAccount:
         else:
             return False
 
-   def display_balance(self):
-    print(f"Current Balance: ${self.__account_balance:.2f}")
+    def display_balance(self):
+        print(f"Current Balance: ${self.__account_balance}") 
+
+
 
         
