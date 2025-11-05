5ac1e797c38609d78f17280ee61f117dd6ab2445
Clement Adjavon <118020602+Nkelechi11@users.noreply.github.com>
2025-11-05 09:07:29 +0000

    Fix variable name and savings calculation formula

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,13 +1,13 @@
 # Declaring variables and asking user for inputs
 monthly_income = float(input("Enter your monthly income: "))
-total_monthly_expenses = float(input("Enter your total monthly expenses: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
 
-monthly_savings = monthly_income - total_monthly_expenses
+monthly_savings = monthly_income - monthly_expenses
 
 # Adding interest rate and making estimations for annual savings
 rate = 0.05
 time = 12
-projected_savings = monthly_savings * 12 + (monthly_savings ** 12 * 0.05)
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
 
 # Display results with formatted values
