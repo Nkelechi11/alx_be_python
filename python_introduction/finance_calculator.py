01a1ecc2f05766530ea3d4c29015e7d023fcf4d2
Clement Adjavon <118020602+Nkelechi11@users.noreply.github.com>
2025-11-05 09:09:48 +0000

    Display error fixed
    Removed commented-out code for projected savings calculation.

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -5,10 +5,6 @@ monthly_expenses = float(input("Enter your total monthly expenses: "))
 monthly_savings = monthly_income - monthly_expenses
 
 # Adding interest rate and making estimations for annual savings
-# rate = 0.05
-# time = 12
-# projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
-
 rate = 0.05
 time = 12
 projected_savings = monthly_savings * time + (monthly_savings * time * rate)
